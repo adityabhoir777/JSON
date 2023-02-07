cfef9198eddc5f8312041b673f2de9dd4f38413e
adityabhoir777 <adityabhoir@mailfence.com>
2023-02-07 08:18:06 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+// 1 
+// Fix the errors in data.json file
+
+
+
+let container=document.getElementById('container');
+
+// 2 
+//Import the data here from data.json
+
+
+// 3
+// For each alien create a list item and add it in container
